22a73c23d9af6edb31a2c488752ea65c17679cd5
Chris <gem6121@dingtalk.com>
2021-11-08 16:50:59 +0800

    master change 4

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("mater change1");
-		System.out.println("hotfix change1");
+		System.out.println("hotfix change1 master");
 		System.out.println("hotfix change2");
 		SpringApplication.run(DemoApplication.class, args);
 	}
