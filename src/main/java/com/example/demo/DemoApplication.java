6db45f36a5d6f8eab35bde156008d19507d51796
accademo1 <93121917+accademo1@users.noreply.github.com>
2021-11-08 17:00:27 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("mater change1");
+		System.out.println("mater change1 at github web");
 		System.out.println("hotfix change1 master");
 		System.out.println("hotfix change2");
 		SpringApplication.run(DemoApplication.class, args);
